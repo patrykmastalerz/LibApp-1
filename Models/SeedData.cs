481935101336f8d1751ec2b2e5420794cb00f71f
Patryk Mastalerz <patryk.mastalerz@microsoft.wsei.edu.pl>
2021-11-27 11:26:34 +0100

    repair SeedData

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -25,30 +25,34 @@ namespace LibApp.Models
                         Id = 1,
                         SignUpFee = 0,
                         DurationInMonths = 0,
-                        DiscountRate = 0
+                        DiscountRate = 0,
+                        Name = "przyklad 1"
                     },
                     new MembershipType
                     {
                         Id = 2,
                         SignUpFee = 30,
                         DurationInMonths = 1,
-                        DiscountRate = 10
+                        DiscountRate = 10,
+                        Name = "przyklad 2"
                     },
                     new MembershipType
                     {
                         Id = 3,
                         SignUpFee = 90,
                         DurationInMonths = 3,
-                        DiscountRate = 15
+                        DiscountRate = 15,
+                        Name = "przyklad 3"
                     },
                     new MembershipType
                     {
                         Id = 4,
                         SignUpFee = 300,
                         DurationInMonths = 12,
-                        DiscountRate = 20
+                        DiscountRate = 20,
+                        Name = "przyklad 4"
                     });
-                //context.SaveChanges();
+                context.SaveChanges();
             }
         }
     }
